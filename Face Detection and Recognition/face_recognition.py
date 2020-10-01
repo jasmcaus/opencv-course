1c3e34c4cfca1752e715fe87e8d46c825071c26d
jasmcaus <61184127+jasmcaus@users.noreply.github.com>
2020-10-01 18:50:11 +0530

    Updated links

--- Face Detection and Recognition/face_recognition.py
+++ Face Detection and Recognition/face_recognition.py
@@ -12,7 +12,7 @@ people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kalin
 face_recognizer = cv.face.LBPHFaceRecognizer_create()
 face_recognizer.read('face_trained.yml')
 
-img = cv.imread(r'C:\Users\jas\Documents\dev\older\Faces\val\elton_john\httpafilesbiographycomimageuploadcfillcssrgbdprgfacehqwMTEODAOTcxNjcMjczMjkzjpg.jpg')
+img = cv.imread(r'../Media Files\Faces\val\elton_john/1.jpg')
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow('Person', gray)
