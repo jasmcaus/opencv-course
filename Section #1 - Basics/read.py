5ae078c2b928e373ba90be78aacdc035d4c57814
1C0D <63018905+1C0D@users.noreply.github.com>
2021-05-26 00:40:23 +0200

    autoclose at the end and no error

--- Section #1 - Basics/read.py
+++ Section #1 - Basics/read.py
@@ -13,9 +13,11 @@ capture = cv.VideoCapture('../Resources/Videos/dog.mp4')
 while True:
     isTrue, frame = capture.read()
     
-    cv.imshow('Video', frame)
-
-    if cv.waitKey(20) & 0xFF==ord('d'):
+    if isTrue:    
+        cv.imshow('Video', frame)
+        if cv.waitKey(20) & 0xFF==ord('d'):
+            break            
+    else:
         break
 
 capture.release()
