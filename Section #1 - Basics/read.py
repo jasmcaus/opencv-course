e42c4b2379078780e13bec5754b18661acbf1018
Jason Dsouza <61184127+jasmcaus@users.noreply.github.com>
2021-05-26 09:41:08 +0530

    Update #15 (@1C0D)

--- Section #1 - Basics/read.py
+++ Section #1 - Basics/read.py
@@ -13,9 +13,15 @@ capture = cv.VideoCapture('../Resources/Videos/dog.mp4')
 while True:
     isTrue, frame = capture.read()
     
-    cv.imshow('Video', frame)
-
-    if cv.waitKey(20) & 0xFF==ord('d'):
+    # if cv.waitKey(20) & 0xFF==ord('d'):
+    # This is the preferred way - if `isTrue` is false (the frame could 
+    # not be read, or we're at the end of the video), we immediately
+    # break from the loop. 
+    if isTrue:    
+        cv.imshow('Video', frame)
+        if cv.waitKey(20) & 0xFF==ord('d'):
+            break            
+    else:
         break
 
 capture.release()
